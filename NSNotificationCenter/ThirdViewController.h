6b4f54fd25f0113a4f627e54d56d2790f720925c
olga-nest <olga.w.nesterova@gmail.com>
2017-10-15 20:29:48 -0700

    Create ThirdViewController class

---
+++ NSNotificationCenter/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  NSNotificationCenter
+//
+//  Created by Olga on 10/15/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
---
+++ NSNotificationCenter/ThirdViewController.m
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.m
+//  NSNotificationCenter
+//
+//  Created by Olga on 10/15/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+
+@end
