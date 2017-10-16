a3a9d79219771b630ab111b7aaf92eace3b3405d
olga-nest <olga.w.nesterova@gmail.com>
2017-10-15 20:51:59 -0700

    Add observer and a method to move the view depending on keyboard size

--- NSNotificationCenter/ThirdViewController.m
+++ NSNotificationCenter/ThirdViewController.m
@@ -9,6 +9,9 @@
 #import "ThirdViewController.h"
 
 @interface ThirdViewController ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomConstraint;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (nonatomic, readonly) CGFloat oldConstraint;
 
 @end
 
@@ -16,9 +19,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    [notificationCenter addObserver:self selector:@selector(keyboardDidChange:) name:UIKeyboardWillChangeFrameNotification object:nil];
 
+    _oldConstraint = self.bottomConstraint.constant;
+}
+
+-(void)keyboardDidChange: (NSNotification *) notification {
+    NSLog(@"%@", notification.userInfo);
+    NSValue *value = notification.userInfo[UIKeyboardFrameEndUserInfoKey];
+    CGRect rect = value.CGRectValue;
+    NSLog(@"%@", NSStringFromCGRect(rect));
+    
+    CGFloat yPos = rect.origin.y;
+    CGFloat viewHeight = self.view.bounds.size.height;
+    
+    if (yPos == viewHeight) {
+        self.bottomConstraint.constant = self.oldConstraint;
+    } else {
+        self.bottomConstraint.constant = viewHeight - yPos;
+    }
+    
+    
+    
 }
 
 
+
+
+
+
+//Add dismiss first responder
 
 @end
